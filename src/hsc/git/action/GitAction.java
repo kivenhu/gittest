584f37cf02a1bc713f22ebbacd030de63ccf8ba2
husc <husc@ecqun.com>
2016-10-08 16:40:35 +0800

    modify action

--- src/hsc/git/action/GitAction.java
+++ src/hsc/git/action/GitAction.java
@@ -16,6 +16,9 @@ public class GitAction {
 
     public static void main(String[] args) {
         for (int i = 0; i < args.length; i++) {
+            if (i % 2 == 0) {
+                System.out.println("this is ");
+            }
 
         }
     }
