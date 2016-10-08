1ea753f491957dfb6419182ee01063d4fab164cc
husc <husc@ecqun.com>
2016-10-08 16:33:57 +0800

    GitAction

---
+++ src/hsc/git/action/GitAction.java
@@ -0,0 +1,22 @@
+/**
+ * @Package hsc.git.action
+ * @Description: TODO
+ * @author hsc
+ * @date 2016-10-8 下午4:30:28
+ */
+package hsc.git.action;
+
+/**
+ * @ClassName: GitAction
+ * @Description: TODO
+ * @author hsc
+ * @date 2016-10-8 下午4:30:28
+ */
+public class GitAction {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+
+        }
+    }
+}
